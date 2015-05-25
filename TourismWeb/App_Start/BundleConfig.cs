4c0cd017cf44b42f8886f3181a50b90f240aacaa
Ron <srl@mail.knu.edu.tw>
2015-05-25 21:21:21 +0800

    renew

---
+++ TourismWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,60 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TourismWeb
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/bower_components/jquery.min.js",
+                         "~/bower_components/moment/min/moment.min.js",
+                         "~/bower_components/fullcalendar/dist/fullcalendar.min.js",
+                         "~/bower_components/chosen/chosen.jquery.min.js",
+                         "~/bower_components/colorbox/jquery.colorbox-min.js",
+                         "~/bower_components/responsive-tables/responsive-tables.js",
+                         "~/bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js",
+                         "~/js/jquery.cookie.js",
+                         "~/js/jquery.dataTables.min.js",
+                         "~/js/jquery.noty.js",
+                         "~/js/jquery.raty.min.js",
+                         "~/js/jquery.iphone.toggle.js",
+                         "~/js/jquery.autogrow-textarea.js",
+                         "~/js/jquery.uploadify-3.1.min.js",
+                         "~/js/jquery.history.js",
+                         "~/js/charisma.js",
+                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap-cerulean.min.css",
+                      "~/css/charisma-app.css",
+                      "~/bower_components/fullcalendar.css",
+                      "~/bower_components/fullcalendar.print.css",
+                      "~/bower_components/chosen.min.css",
+                      "~/bower_components/colorbox.css",
+                      "~/bower_components/responsive-tables.css",
+                      "~/bower_components/bootstrap-tour.min.css",
+                      "~/css/jquery.noty.css",
+                      "~/css/noty_theme_default.css",
+                      "~/css/elfinder.min.css",
+                      "~/css/elfinder.theme.css",
+                      "~/css/jquery.iphone.toggle.css",
+                      "~/css/uploadify.css",
+                      "~/css/animate.min.css"));
+        }
+    }
+}
---
+++ TourismWeb/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TourismWeb.Models
+{
+    // 您可以在 ApplicationUser 類別新增更多屬性，為使用者新增設定檔資料，請造訪 http://go.microsoft.com/fwlink/?LinkID=317594 以深入了解。
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // 注意 authenticationType 必須符合 CookieAuthenticationOptions.AuthenticationType 中定義的項目
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在這裡新增自訂使用者宣告
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<ApplicationUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("MyUserRoles");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("MyUserLogins");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("MyUserClaims");
+            modelBuilder.Entity<IdentityRole>().ToTable("MyRoles");
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
