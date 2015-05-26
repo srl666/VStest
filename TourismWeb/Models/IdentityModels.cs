072fe1f1c90bcca9ac14c89977afffa49c69812f
Ron <srl@mail.knu.edu.tw>
2015-05-26 20:32:54 +0800

    commit test

--- TourismWeb/Models/IdentityModels.cs
+++ TourismWeb/Models/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace TourismWeb.Models
 {
@@ -20,26 +22,51 @@ namespace TourismWeb.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        static ApplicationDbContext()
+        {
+            Database.SetInitializer<ApplicationDbContext>(null);
+        }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
+        public virtual DbSet<Blog> Blogs { get; set; }
+
         protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            
+            
+            modelBuilder.Entity<IdentityUser>().ToTable("MyUsers", "dbo").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<ApplicationUser>().ToTable("MyUsers", "dbo").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("MyUserRoles", "dbo");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("MyUserLogins", "dbo");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("MyUserClaims", "dbo");
+            modelBuilder.Entity<IdentityRole>().ToTable("MyRoles", "dbo");
 
-            modelBuilder.Entity<IdentityUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
-            modelBuilder.Entity<ApplicationUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
-            modelBuilder.Entity<IdentityUserRole>().ToTable("MyUserRoles");
-            modelBuilder.Entity<IdentityUserLogin>().ToTable("MyUserLogins");
-            modelBuilder.Entity<IdentityUserClaim>().ToTable("MyUserClaims");
-            modelBuilder.Entity<IdentityRole>().ToTable("MyRoles");
+            modelBuilder.Entity<Blog>().ToTable("BlogMainFiles").Property(p => p.Id);
+            //var blogTable = modelBuilder.Entity<Blog>().ToTable("BlogMainFiles");
+
+           // blogTable
+           //     .Property(c => c.Id)
+           //     .IsRequired()
+           //     .HasColumnType("bigint");
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public class Blog
+        {
+            [Key]
+            [Required]
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
     }
 }
